79abb879c209332dd96029a5eca61d4bd5fa3996
Andreas Petutschnig <andreas.petutschnig@adesso.de>
2025-01-16 13:22:55 +0000

    fix BECS area calculation

--- R/becs.R
+++ R/becs.R
@@ -1,13 +1,10 @@
 calc_becs <- function(indicators) {
-  indicators <- mapme.biodiversity::portfolio_wide(indicators,
+  indicators <- mapme.biodiversity::portfolio_wide(indicators[, -which(names(indicators) == "org_geometry")],
                                                    indicators = c("mean_species_abundance", "star"))
-  indicators$proximity_kba <- NULL
-  indicators$proximity_wdpa <- NULL
-  indicators$species_richness <- NULL
-  indicators$`star_2019-01-01_restoration_max_unitless` <- NULL
+
   names(indicators)[grepl("abatement_max", names(indicators))] <- "abatement_value"
   names(indicators)[grepl("mean_species_abundance", names(indicators))] <- "mean_species_abundance"
-  indicators$area_km2 <- as.numeric(units::set_units(sf::st_area(sf::st_sf(indicators)), "km2"))
+  indicators$area_km2 <- as.numeric(units::set_units(sf::st_area(indicators), "km2"))
 
   indicators$becs_extent <- log10(indicators$area_km2)
   indicators$becs_condition <- indicators$mean_species_abundance
